71d2f8f3da681f1fca4fb1e9a2e2c2ef6e10259b
Andrej Holeša <54326310+holesa@users.noreply.github.com>
2021-02-06 21:32:21 +0100

    add castle scene

--- poly/app.js
+++ poly/app.js
@@ -1,5 +1,3 @@
-const loader = new THREE.GLTFLoader();
-const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
   75,
   window.innerWidth / window.innerHeight,
@@ -7,22 +5,32 @@ const camera = new THREE.PerspectiveCamera(
   1000
 );
 
-const renderer = new THREE.WebGLRenderer({ antialias: true });
-
-renderer.setSize(window.innerWidth, window.innerHeight);
-renderer.setClearColor(0xffffff, 1);
-document.getElementById("mountain").appendChild(renderer.domElement);
-
 camera.position.y = 10;
 camera.position.z = 60;
 
-loader.load(
-  "https://holesa.github.io/staging.github.io/poly/models/mountains.glb",
+// // Model - plane
+const loader1 = new THREE.GLTFLoader();
+const scene1 = new THREE.Scene();
+const renderer1 = new THREE.WebGLRenderer({ antialias: true });
+
+const lightA1 = new THREE.AmbientLight(0xced0de); // soft white light
+scene1.add(lightA1);
+
+const lightP1 = new THREE.PointLight(0xdae5e8, 0.5);
+lightP1.position.set(-10, 30, 700);
+scene1.add(lightP1);
+
+renderer1.setSize(window.innerWidth, window.innerHeight);
+renderer1.setClearColor(0xdfe4e8, 1.07);
+document.getElementById("plane").appendChild(renderer1.domElement);
+
+loader1.load(
+  "https://holesa.github.io/staging/poly/models/castle/castle.gltf",
   function (glb) {
-    glb.scene.scale.set(0.01, 0.01, 0.01);
-    glb.scene.rotateX(101);
+    glb.scene.scale.set(0.38, 0.38, 0.38);
+    glb.scene.rotateX(100.7);
     glb.scene.rotateY(0.2);
-    scene.add(glb.scene);
+    scene1.add(glb.scene);
   },
   function (xhr) {
     console.log((xhr.loaded / xhr.total) * 100 + "% loaded");
@@ -32,27 +40,62 @@ loader.load(
   }
 );
 
-const lightA = new THREE.AmbientLight(0x404040); // soft white light
-scene.add(lightA);
+const animate1 = function () {
+  requestAnimationFrame(animate1);
+  renderer1.setPixelRatio(window.devicePixelRatio);
+  renderer1.render(scene1, camera);
+};
 
-const lightP = new THREE.PointLight(0x557bc9, 1);
-lightP.position.set(-10, 30, 500);
-scene.add(lightP);
+animate1();
+
+// Model - mountains
+const loader2 = new THREE.GLTFLoader();
+const scene2 = new THREE.Scene();
+const renderer2 = new THREE.WebGLRenderer({ antialias: true });
+renderer2.setSize(window.innerWidth, window.innerHeight);
+renderer2.setClearColor(0xffffff, 1);
+document.getElementById("mountain").appendChild(renderer2.domElement);
+
+loader2.load(
+  "https://holesa.github.io/staging/poly/models/mountains.glb",
+  function (glb) {
+    glb.scene.scale.set(0.01, 0.01, 0.01);
+    glb.scene.rotateX(101);
+    glb.scene.rotateY(0.2);
+    scene2.add(glb.scene);
+  },
+  function (xhr) {
+    console.log((xhr.loaded / xhr.total) * 100 + "% loaded");
+  },
+  function (error) {
+    console.log("An error happened");
+  }
+);
+
+const lightA2 = new THREE.AmbientLight(0x404040); // soft white light
+scene1.add(lightA2);
+
+const lightP2 = new THREE.PointLight(0x557bc9, 1);
+lightP2.position.set(-10, 30, 500);
+
+scene2.add(lightA2);
+
+scene2.add(lightP2);
 
 var mouseTolerance = 0.3;
 
-document.onmousemove = function (e) {
-  var centerX = window.innerWidth * 0.4;
-  var centerY = window.innerHeight * 0.4;
+// document.onmousemove = function (e) {
+//   var centerX = window.innerWidth * 0.4;
+//   var centerY = window.innerHeight * 0.4;
 
-  //scene.rotation.y = ((e.clientX - centerX) / centerX) * mouseTolerance;
-  scene.rotation.x = ((e.clientY - centerY) / centerY) * mouseTolerance;
+//   //scene.rotation.y = ((e.clientX - centerX) / centerX) * mouseTolerance;
+//   scene2.rotation.x = ((e.clientY - centerY) / centerY) * mouseTolerance;
+// };
+
+const animate2 = function () {
+  requestAnimationFrame(animate2);
+  renderer2.setPixelRatio(window.devicePixelRatio);
+  renderer2.render(scene2, camera);
 };
 
-const animate = function () {
-  requestAnimationFrame(animate);
-  renderer.setPixelRatio(window.devicePixelRatio);
-  renderer.render(scene, camera);
-};
-
-animate();
+animate2();
